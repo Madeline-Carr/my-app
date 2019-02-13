acaf62bbdc89c8da845f768c84b463018b822360
Madeline Carr <madelinencarr@Gmail.com>
2019-02-12 22:33:15 -0500

    Removed Random Content and Created Header
    I removed unused files, added a header, and deleted practice and placeholder code.

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>I am a header!</h1>
+            </div>
+        );
+    }
+}
+export default Header;
