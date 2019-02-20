1a82a2962e120b9450ff3e3f1f7ca44bdbad8cb3
Madeline Carr <madelinencarr@Gmail.com>
2019-02-19 23:12:56 -0500

    Added a Method To the Article Class
    This is an example of how to add a method to a class.

--- src/components/article.js
+++ src/components/article.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 class Article extends React.Component {
+
+    getText() {
+        return (<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum quas veniam dolore? Omnis, perferendis quasi quo dolor consectetur deleniti nobis sed quod recusandae delectus ex consequuntur praesentium, facere atque vitae.</p>);
+    }
+
+
     render() {
         return (
             <div className="container">
                 <h2>{this.props.articleTitle}</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum quas veniam dolore? Omnis, perferendis quasi quo dolor consectetur deleniti nobis sed quod recusandae delectus ex consequuntur praesentium, facere atque vitae.</p>
+                <p>{this.getText()}</p>
             </div>
         );
     }
