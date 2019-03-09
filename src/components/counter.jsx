49e9aa34489bbea45b1b931883ef572dee0a93e0
Madeline Carr <madelinencarr@Gmail.com>
2019-03-09 14:15:06 -0500

    Created the Counter Component

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Hello!</h2>
+                <button>Increment</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
