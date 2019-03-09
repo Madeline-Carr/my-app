f64fe1476c1dea000cac64c15a30f815c101b645
Madeline Carr <madelinencarr@Gmail.com>
2019-03-09 13:47:05 -0500

    Added Bootstrap and Reset Files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Header from "./components/header";
-import Article from "./components/article"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-    <div>
-        <Header name="Maddie" />
-        <Header name="David" />
-        <Article articleTitle="I am an article title!" />
-        <Article articleTitle="I am an another!" />
-    </div>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
