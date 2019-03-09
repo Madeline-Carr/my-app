8cf110bd275c6523f101618910c3cd2db94acf14
Madeline Carr <madelinencarr@Gmail.com>
2019-03-09 14:15:54 -0500

    Added State to the Counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-
+    state = {
+        count: 0,
+    };
     render() {
         return (
             <div>
                 <h2>Hello!</h2>
+                <span>{this.state.count}</span>
                 <button>Increment</button>
             </div>
         );
