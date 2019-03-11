e84918a758d2bb84f04bd50ca1ba5582de403594
Madeline Carr <madelinencarr@Gmail.com>
2019-03-10 20:22:23 -0400

    Adding Styles to Elements
    I added styles to the elements and I added an image.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        imageURL: 'https:picsum.photos/200'
     };
     render() {
         return (
             <div>
                 <h2>Hello!</h2>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <img src={this.state.imageURL} alt="" />
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-danger btn-sm">Increment</button>
             </div>
         );
     }
