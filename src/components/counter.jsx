d4290d7bff3f01bbeb4a5f7a2044e5935a3fc071
Madeline Carr <madelinencarr@Gmail.com>
2019-03-10 20:44:02 -0400

    Refactored getBadgeClasses

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 8,
         imageURL: 'https:picsum.photos/200'
     };
     render() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
         return (
             <div>
                 <img src={this.state.imageURL} alt="" />
-                <span className={classes}>{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-danger btn-sm">Increment</button>
             </div>
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount() {
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
