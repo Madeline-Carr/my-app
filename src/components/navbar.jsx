9bdd7c322acb743b19c38ac3ce40ea200a3bce03
Madeline Carr <madelinencarr@Gmail.com>
2019-03-14 18:14:24 -0400

    Added a Badge with the Number of Counters

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,10 +4,12 @@ class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="facebook.com">Navbar</a>
+                <a className="navbar-brand" href="facebook.com">Navbar
+                    <span className="badge badge-pill badge-secondary m-2">{this.props.totalCounters}</span>
+                </a>
             </nav>
         );
     }
 }
 
-export default NavBar;
+export default NavBar;  
